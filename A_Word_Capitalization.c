f7cf0b771cdfffd2400b0f3b4155ab1136fa292a
ACC-ASS-2024-00030 <webappick-acc-ass-2024-00030@SHOJOL-ISLAM.local>
2025-05-23 11:22:20 +0600

    A. Word Capitalization

---
+++ A_Word_Capitalization.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  char word[1001];
+  scanf("%s", word);
+  if (word[0] >= 97 && word[0] <= 122)
+  {
+    word[0] = word[0] - 32;
+  }
+  printf("%s", word);
+
+  return 0;
+}
